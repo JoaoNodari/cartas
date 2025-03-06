a7e9c1452e77020e0b9282e7afa29d018be4b27d
João Nodari <joaonodari2017@hotmail.com>
2025-03-06 17:29:23 -0300

    Atualização css.

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const setaAvancar = document.querySelector('.btn-avancar')
+const setaVoltar = document.querySelector('.btn-voltar')
+
+setaAvancar.addEventListener('click', () => {
+    console.log('oi')
+})
