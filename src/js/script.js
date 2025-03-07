03383e5e4607ed0b59425f0412229a93319a1c81
João Nodari <joaonodari2017@hotmail.com>
2025-03-07 16:50:33 -0300

    Atualização do Projeto.

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,28 @@
 const setaAvancar = document.querySelector('.btn-avancar')
 const setaVoltar = document.querySelector('.btn-voltar')
 
+const cartoes = document.querySelectorAll('.cartao')
+let cartaoAtual = 0
+
 setaAvancar.addEventListener('click', () => {
-    console.log('oi')
+    if(cartaoAtual === cartoes.length - 1) return
+    cartaoSelecionado()
+    cartaoAtual++
+    adicionarClasse(cartaoAtual)
 })
+
+setaVoltar.addEventListener('click', () => {
+    if(cartaoAtual === 0) return
+    cartaoSelecionado()
+    cartaoAtual--
+    adicionarClasse(cartaoAtual)
+})
+
+function cartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector('.selecionado') // Identificando a classe que tem o selecionado
+    cartaoSelecionado.classList.remove('selecionado') // Removendo a classe selecionado
+}
+
+function adicionarClasse(cartaoAtual) {
+    cartoes[cartaoAtual].classList.add('selecionado')
+}
